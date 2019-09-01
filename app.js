872ad2193463b7c7cf714b03eafb8260f5d04995
Ewa Czech <ewa.czech@hotmail.com>
2019-09-01 17:53:58 +0200

    add modal/lightbox functionality

---
+++ app.js
@@ -0,0 +1,99 @@
+const containerElement = document.querySelector('.container');
+const url = 'https://randomuser.me/api/?results=12&nat=us';
+
+// ------------------------------------------
+//  FETCH FUNCTION
+// ------------------------------------------
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    generateEmployeeCard(data);
+  })
+  .catch(error => console.log('error is', error));
+
+function generateEmployeeCard(data) {
+
+  let listOfEmployees = data.results;
+  let modal;
+  listOfEmployees.map( (item, index) => {
+    let employeeElement = document.createElement("div");
+
+    let date = item.dob.date.slice(0, 10).replace(/-/g,"/");
+    let firstName = item.name.first.charAt(0).toUpperCase() + item.name.first.slice(1);
+    let lastName = item.name.last.charAt(0).toUpperCase() + item.name.last.slice(1);
+    let city = item.location.city.charAt(0).toUpperCase() + item.location.city.slice(1);
+
+
+    employeeElement.classList.add("employee");
+    employeeElement.setAttribute('data-index', `${index+1}`);
+
+    employeeElement.innerHTML = `<img src="${item.picture.large}" alt="" class="employee__photo" data-index= ${index+1}>
+    <ul data-index = ${index+1}>
+      <li class="employer__name" data-index = ${index+1}>${firstName} ${lastName} </li>
+      <li class="employer__mail" data-index = ${index+1}>${item.email}</li>
+      <li class="employer__location" data-index = ${index+1}>${city}</li>
+    </ul>
+    `;
+
+    modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.setAttribute('data-index', `${index+1}`);
+    modal.style.display = 'none';
+    modal.innerHTML = `
+      <div class="arrow-left" data-index = ${index+1}>&lt;</div><div class="modal-content" data-index = ${index+1} >
+        <div>
+          <span class="close-modal">X</span>
+        </div>
+
+        <img src="${item.picture.large}" alt="" class="modal__photo">
+        <ul>
+          <li class="modal__name">${firstName} ${lastName}</li>
+          <li class="modal__mail">${item.email}</li>
+          <li class="modal__location">${city}</li>
+          <hr/>
+          <li class="modal__phone">${item.phone}</li>
+          <li class="modal__address">${item.location.street} ${item.location.postcode}</li>
+          <li class="modal__birthday">Birthday: ${date}</li>
+        </ul>
+
+      </div><div class="arrow-right" data-index = ${index+1}>&gt;</div>
+    `;
+    employeeElement.appendChild(modal);
+    containerElement.appendChild(employeeElement);
+
+  })
+
+  containerElement.addEventListener('click', function(e) {
+    let index = e.target.getAttribute('data-index');
+    const modalsCollection = document.querySelectorAll('.modal');
+
+    for(let i = 0; i < modalsCollection.length; i++) {
+
+      if(modalsCollection[i].getAttribute('data-index') === index) {
+        modalsCollection[i].style.display = 'flex';
+      } else {
+        modalsCollection[i].style.display = 'none';
+      }
+    }});
+
+  const modalsCollection = document.querySelectorAll('.modal');
+  for(let i = 0 ; i < modalsCollection.length; i++) {
+    modalsCollection[i].addEventListener('click', function(e) {
+      let index = e.target.getAttribute('data-index');
+      if(e.target.nodeName === 'SPAN') {
+        modalsCollection[i].style.display = 'none';
+      } else if (e.target.classList.contains('arrow-left')) {
+        e.stopPropagation();
+        modalsCollection[i].style.display = 'none';
+        modalsCollection[i-1].style.display = 'flex';
+      } else if (e.target.classList.contains('arrow-right')) {
+        e.stopPropagation();
+        modalsCollection[i].style.display = 'none';
+        modalsCollection[i+1].style.display = 'flex';
+      }
+    });
+  }
+};
+
+generateEmployeeCard(url);
